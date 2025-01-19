4b54225e60e884fd220d21180b5fe084d5a9657c
CarPasBosch <carlospascualbosch@hotmail.com>
2025-01-19 18:41:27 +0000

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Gestión de Inventario y Ventas")
+
+with st.sidebar:
+    st.header("Nueva Venta")
+    fecha = st.date_input("Fecha")
+    producto = st.text_input("Producto")
+    cantidad = st.number_input("Cantidad", min_value=1, step=1)
+    precio = st.number_input("Precio", min_value=0.0, step=0.01)
+    metodo_pago = st.selectbox("Método de pago", ["bizum", "efectivo", "tarjeta"])
+
